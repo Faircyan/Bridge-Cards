691f9ae3c3a4e81e56b01c332ce229345fe726c8
faircyan <09fatihhamza@gmail.com>
2019-05-03 19:22:20 +0300

    Random card Turn add

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CardScript : MonoBehaviour {
 
     public GameObject cardHand;
     public GameObject cardBack;
     private bool isTurnBack = false;
+    private bool isOpened = false;
     private bool isTurnHand = false;
+    public int counter = 14;
 
+  
     private void Start()
     {
-        cardBack.transform.localScale = new Vector3(0, 1, 1);
+        cardHand.transform.localScale = new Vector3(0, 1, 1);
     }
-
 
     void Update () {
 
@@ -30,6 +30,7 @@ public class CardScript : MonoBehaviour {
         {
             cardBack.transform.localScale = new Vector3(1f, 1f, 1f);
             isTurnBack = false;
+            isOpened = false;
         }
 
         else if (isTurnHand && cardBack.transform.localScale.x > 0f)
@@ -45,6 +46,7 @@ public class CardScript : MonoBehaviour {
         {
             isTurnHand = false;
             cardHand.transform.localScale = new Vector3(1f, 1f, 1f);
+            isOpened = true;
         }
         }
 
@@ -52,11 +54,46 @@ public class CardScript : MonoBehaviour {
     public void TurnBack()
     {
         isTurnBack = true;
+        GameObject.Find("Random").GetComponent<CardScript>().IncreaseCounter();
     }
 
     public void TurnHand()
     {
         isTurnHand = true;
+        GameObject.Find("Random").GetComponent<CardScript>().DecreaseCounter();
+
     }
-   
+  
+    public bool getIsOpened()
+    {
+        return isOpened;
+    }
+
+    private void DecreaseCounter()
+    {
+        counter--;
+
+        if (counter == 0)
+            GameObject.Find("Random").SetActive(false);
+    }
+
+    public void IncreaseCounter()
+    {
+        counter++;
+    }
+
+    public void TurnRandomCard()
+    {
+        System.Random random = new System.Random();
+        bool result = true;
+        GameObject card = new GameObject();
+        while (result)
+        {
+            int index = random.Next(2, 16);
+            card = GameObject.Find("Card" + index.ToString());
+            result = card.GetComponentInChildren<CardScript>().getIsOpened();
+        }
+        card.GetComponentInChildren<CardScript>().TurnHand();
+        Destroy(GameObject.Find("New Game Object"));
+    }
 }
